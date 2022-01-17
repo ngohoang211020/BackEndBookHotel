f289082f12e85989ce3d133f49530e3339c7da1f
ngohoang2110 <hoanggg2110@gmail.com>
2022-01-17 17:00:37 +0700

    Deploy Heroku

---
+++ src/main/java/com/bookhotel/entity/Comment.java
@@ -0,0 +1,26 @@
+package com.bookhotel.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/*@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")*/
+public class Comment {
+/*    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")    private Integer id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;*/
+}
