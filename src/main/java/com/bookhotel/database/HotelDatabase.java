5c821c887beff759c1493fbce909eacb32fa7ca0
ngohoang2110 <hoanggg2110@gmail.com>
2022-01-17 17:48:32 +0700

    Deploy Heroku

--- src/main/java/com/bookhotel/database/HotelDatabase.java
+++ src/main/java/com/bookhotel/database/HotelDatabase.java
@@ -15,10 +15,10 @@ import java.util.List;
 
 @Configuration
 public class HotelDatabase {
-   /* @Autowired
-    private RoleRepository roleRepository;
     @Autowired
-    private LocationRepository locationRepository;
+    private RoleRepository roleRepository;
+   /* @Autowired
+    private LocationRepository locationRepository;*/
     @Bean
     CommandLineRunner initDatabase() {
         return new CommandLineRunner() {
@@ -31,7 +31,7 @@ public class HotelDatabase {
                 list.add(roleAdmin); list.add(roleMod); list.add(roleUser);
                 roleRepository.saveAll(list);
 
-           List<Location> locations=new ArrayList<Location>();
+         /*  List<Location> locations=new ArrayList<Location>();
                 locations.add(new Location(1,"Đà Nẵng","https://vcdn1-dulich.vnecdn.net/2020/07/01/shutterstock1169930359-1593591-8866-7555-1593591174.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=lSSNsc9gxo-3y6TVbCQAJQ"));
                 locations.add(new Location(2,"Sài Gòn","https://vietsensetravel.com/view/at_gioi-thieu-ve-sai-gon_3ef0af3f1d84ab6cf38ed014a5fab4b7.jpg"));
                 locations.add(new Location(3,"Hà Nội","https://owa.bestprice.vn/images/destinations/uploads/trung-tam-thanh-pho-ha-noi-603da1f235b38.jpg"));
@@ -39,9 +39,9 @@ public class HotelDatabase {
                 locations.add(new Location(5,"Đà Lạt","https://znews-photo.zadn.vn/w660/Uploaded/qhj_pwqvdvicbu/2019_03_23/THUMB.jpg"));
                 locations.add(new Location(6,"Hội An","https://dulichannam.com/wp-content/uploads/2018/10/hoi_an_dem.jpg"));
                 locationRepository.saveAll(locations);
-
+*/
             }
         };
 
-    }*/
+    }
 }
